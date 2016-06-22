9257ce28de47544095a2da4c9fef5c125e6e9550
paul <paul@wizmacau.com>
2016-06-22 15:33:28 +0800

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,14 +6,20 @@ if [ "${1:0:1}" = '-' ]; then
 	set -- glide "$@"
 fi
 
-if [ "${1:0:2}" = 'go' ]; then
-  
+# update git resource
+if git pull > /dev/null 2>&1; then
+  >&2 echo "updating git resource -- git pull"
+	set -- git pull
+fi
+
+# update git resource
+if [ "$1" = 'go' ]; then
   until glide update '\l'; do
-    >&2 echo "Git Resource unavaliable!"
+    >&2 echo "updating go vendor -- glide update"
     sleep 1
   done
   
-  >&2 echo "Git Resource avaliable!"
+  >&2 echo "Start Go Server..."
   
   set -- "$@"
 fi
